9570bed46b23aa2ca2cfac403dca53fd46ae60b1
Louix Abrenica <pocholo.abrenica@obf.ateneoe.edu>
2020-12-22 15:43:59 -0800

    Fixed scrolling issue on LoginPage.dart and added navigation to Dashboard

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:patient_tracker/DashBoard.dart';
+
 
 class LoginPage extends StatefulWidget {
   @override
@@ -10,54 +12,71 @@ class _LoginPage extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 35),
-            child: TextFormField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                labelText: 'Username',
-                prefixIcon: Icon(Icons.account_circle)
+      body: SingleChildScrollView(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 150, top: 150),
+              child: Image(
+                image: AssetImage('assets/sample_icon.png'),
+                height: 150,
+                width: 150,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 35),
-            child: TextFormField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                labelText: 'Password',
-                prefixIcon: Icon(Icons.lock_outline) ,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 35),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: 'Username',
+                  prefixIcon: Icon(Icons.account_circle)
+                ),
               ),
-              obscureText: true,
             ),
-          ),
-          SizedBox(height: 50,),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FlatButton(
-                  onPressed: (){},
-                  splashColor: Colors.white,
-                  highlightColor: Colors.white,
-                  child: Text('Sign Up'),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 35),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock_outline) ,
                 ),
-                RaisedButton(
-                  textColor: Colors.white,
-                  color: Theme.of(context).primaryColor,
-                  onPressed: (){},
-                  child: Text('Login'),
-                ),
-            ],),
-          ),
-        ],
+                obscureText: true,
+              ),
+            ),
+            SizedBox(height: 50,),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FlatButton(
+                    onPressed: (){},
+                    splashColor: Colors.white,
+                    highlightColor: Colors.white,
+                    child: Text('Sign Up'),
+                  ),
+                  RaisedButton(
+                    textColor: Colors.white,
+                    color: Theme.of(context).primaryColor,
+                    onPressed: (){
+                      navigateToDashboard(context);
+                    },
+                    child: Text('Login'),
+                  ),
+              ],),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+void navigateToDashboard(BuildContext context) {
+  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => DashBoard()));
 }
